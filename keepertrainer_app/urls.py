5b8d62bc4822dfa2ab5e0066d834c1f5a388cd21
matthiashugli <development@keepertrainer.ch>
2018-01-17 21:17:05 +0100

    refactored password reset email

--- keepertrainer_app/urls.py
+++ keepertrainer_app/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'', include('trainerapp.urls')),
     url(r'^accounts/', include('registration.backends.default.urls')),
+    url(r'^auth/', include('django.contrib.auth.urls')),
     url(r'^admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
