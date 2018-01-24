c5eaa1017f7c880bbd13333caf69b887f70c060b
matthiashugli <development@keepertrainer.ch>
2018-01-24 20:42:28 +0100

    refactored header and footer

--- keepertrainer_app/urls.py
+++ keepertrainer_app/urls.py
@@ -17,10 +17,12 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     url(r'', include('trainerapp.urls')),
+    url(r'^accounts/login/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
     url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^', include('django.contrib.auth.urls')),
     url(r'^admin/', admin.site.urls),
