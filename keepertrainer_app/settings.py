7672c41a92cd2e41b759b6359f3804ca4823da4c
matthiashugli <development@keepertrainer.ch>
2018-02-06 22:43:15 +0100

    fixed unicode import

--- keepertrainer_app/settings.py
+++ keepertrainer_app/settings.py
@@ -14,8 +14,9 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 # -*- coding: utf-8 -*-
 
-import os
 from __future__ import unicode_literals
+import os
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
